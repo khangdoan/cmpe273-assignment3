632393b88486a9007f2bcee7635c77576c7ac67d
Khang K. Doan <khang.doan@sjsu.edu>
2020-05-07 22:54:06 -0700

    Update cache_client.py

--- cache_client.py
+++ cache_client.py
@@ -1,16 +1,15 @@
-import sys
 import socket
 
 from sample_data import USERS
 from server_config import NODES
 from pickle_hash import serialize_GET, serialize_PUT, serialize_DELETE
 from node_ring import NodeRing
-from LRUCache import LRUCache
-from bloomfilter import BloomFilter
+# from LRUCache import LRUCache
+from bloom_filter import BloomFilter
+from lru_cache import lru_cache
+
 BUFFER_SIZE = 1024
-lru_get = None
-lru_put = None
-lru_delete = None
+
 bloomfilter = BloomFilter(1000,0.001)
 
 class UDPClient():
@@ -30,48 +29,6 @@ class UDPClient():
             exit()
 
 
-def lru_cache(size=2):
-    def inner(func):
-        def wrapper(*args, **kwargs):
-            if func.__name__  == 'get':
-                global lru_get
-                if lru_get is None:
-                    lru_get = LRUCache(size)
-                if args[0] in lru_get.dictCache:
-                    return lru_get[args[0]]
-                else:
-                    out = func(*args, **kwargs)
-                    lru_get[args[0]] = out
-                    return out
-                pass
-            elif func.__name__ == 'put':
-                global lru_put
-                if lru_put is None:
-                    lru_put = LRUCache(size)
-                hashCode = serialize_PUT(args[0])[1]
-                if hashCode in lru_put.dictCache:
-                    return lru_put[hashCode]
-                else:
-                    out = func(*args, **kwargs)
-                    lru_put[hashCode] = out
-                    return out
-            elif func.__name__ == 'delete':
-                global lru_delete
-                if lru_delete is None:
-                    lru_delete = LRUCache(size)
-                if args[0] in lru_delete.dictCache:
-                    return lru_delete[args[0]]
-                else:
-                    out = func(*args, **kwargs)
-                    lru_delete[args[0]] = out
-                    return out
-            else:
-                pass
-            return func(*args, **kwargs)
-        return wrapper
-
-    return inner
-
 @lru_cache(5)
 def get(id, clientRing):
     data, key = serialize_GET(id)
@@ -81,14 +38,14 @@ def get(id, clientRing):
         return None
 
 
-@lru_cache(5)
+# @lru_cache(5)
 def put(object, clientRing):
     data, key = serialize_PUT(object)
     bloomfilter.add(key)
     return clientRing.get_node(key).send(data)
 
 
-@lru_cache(5)
+# @lru_cache(5)
 def delete(id, clientRing):
     data, key = serialize_DELETE(id)
     if bloomfilter.is_member(key):
@@ -99,6 +56,9 @@ def delete(id, clientRing):
 
 def process(udp_clients):
     client_ring = NodeRing(udp_clients)
+    # weightList = [500,501,502,503]
+    # seedList = [100, 200, 300, 400]
+    # client_ring = NodeRing(udp_clients,mode='Rendezvous Hashing',weights=weightList, seeds=seedList)
     hash_codes = set()
     # PUT all users.
     for u in USERS:
